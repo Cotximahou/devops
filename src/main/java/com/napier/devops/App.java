d1b0bcc658cc022156dffb9a9383d6361d27e543
Miguel Gonzalez Sanchez <40800669@live.napier.ac.uk>
2025-09-30 12:57:43 +0100

    changes into the application

--- src/main/java/com/napier/devops/App.java
+++ src/main/java/com/napier/devops/App.java
@@ -4,7 +4,25 @@ import java.sql.*;
 
 public class App
 {
+    // Connection to the database
+    private Connection con = null;
+
     public static void main(String[] args)
+    {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Disconnect from database
+        a.disconnect();
+    }
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
     {
         try
         {
@@ -17,9 +35,7 @@ public class App
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
+        int retries = 10;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
@@ -27,22 +43,25 @@ public class App
             {
                 // Wait a bit for db to start
                 Thread.sleep(30000);
-                // Connect to database with proper parameters
+
+                // Connect to database
                 con = DriverManager.getConnection(
-                        "jdbc:mysql://db:3306/employees?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC",
-                        "appuser",
-                        "apppass"
+                        "jdbc:mysql://db:3306/employees?useSSL=false",
+                        "root",
+                        "example"
                 );
+
                 System.out.println("Successfully connected");
+
                 // Wait a bit
                 Thread.sleep(10000);
+
                 // Exit for loop
                 break;
             }
-
             catch (SQLException sqle)
             {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println("Failed to connect to database attempt " + i);
                 System.out.println(sqle.getMessage());
             }
             catch (InterruptedException ie)
@@ -50,7 +69,13 @@ public class App
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
         if (con != null)
         {
             try
@@ -58,7 +83,7 @@ public class App
                 // Close connection
                 con.close();
             }
-            catch (Exception e)
+            catch (SQLException e)
             {
                 System.out.println("Error closing connection to database");
             }
