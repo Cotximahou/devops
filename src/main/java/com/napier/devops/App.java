2f642f9a9645f98536923f252d14cbd141b6f685
Miguel Gonzalez Sanchez <40800669@live.napier.ac.uk>
2025-09-30 16:44:31 +0100

    Adding new feature, salaries by role

--- src/main/java/com/napier/devops/App.java
+++ src/main/java/com/napier/devops/App.java
@@ -1,6 +1,8 @@
 package com.napier.devops;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App
 {
@@ -14,10 +16,14 @@ public class App
 
         // Connect to database
         a.connect();
+
         // Get Employee
         Employee emp = a.getEmployee(255530);
-        // Display results
         a.displayEmployee(emp);
+
+        // Example: Get all Engineers and their salaries
+        List<Employee> engineers = a.getSalariesByRole("Engineer");
+        a.displaySalariesByRole(engineers);
 
         // Disconnect from database
         a.disconnect();
@@ -30,7 +36,6 @@ public class App
     {
         try
         {
-            // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
         }
         catch (ClassNotFoundException e)
@@ -45,18 +50,14 @@ public class App
             System.out.println("Connecting to database...");
             try
             {
-                // Wait a bit for db to start
                 Thread.sleep(30000);
-                // Connect to database with proper parameters
                 con = DriverManager.getConnection(
                         "jdbc:mysql://db:3306/employees?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC",
                         "appuser",
                         "apppass"
                 );
                 System.out.println("Successfully connected");
-                // Wait a bit
                 Thread.sleep(10000);
-                // Exit for loop
                 break;
             }
             catch (SQLException sqle)
@@ -80,7 +81,6 @@ public class App
         {
             try
             {
-                // Close connection
                 con.close();
             }
             catch (SQLException e)
@@ -89,51 +89,127 @@ public class App
             }
         }
     }
+
+    /**
+     * Get a single employee by ID
+     */
     public Employee getEmployee(int ID)
     {
         try
         {
-            // Create an SQL statement
-            Statement stmt = con.createStatement();
-            // Create string for SQL statement
             String strSelect =
-                    "SELECT emp_no, first_name, last_name "
-                            + "FROM employees "
-                            + "WHERE emp_no = " + ID;
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-            // Return new employee if valid.
-            // Check one is returned
+                    "SELECT e.emp_no, e.first_name, e.last_name, " +
+                            "       t.title, s.salary, d.dept_name, " +
+                            "       CONCAT(m.first_name, ' ', m.last_name) AS manager " +
+                            "FROM employees e " +
+                            "JOIN dept_emp de ON e.emp_no = de.emp_no " +
+                            "JOIN departments d ON de.dept_no = d.dept_no " +
+                            "JOIN titles t ON e.emp_no = t.emp_no " +
+                            "JOIN salaries s ON e.emp_no = s.emp_no " +
+                            "JOIN dept_manager dm ON d.dept_no = dm.dept_no " +
+                            "JOIN employees m ON dm.emp_no = m.emp_no " +
+                            "WHERE e.emp_no = ? " +
+                            "  AND de.to_date = '9999-01-01' " +
+                            "  AND t.to_date = '9999-01-01' " +
+                            "  AND s.to_date = '9999-01-01' " +
+                            "  AND dm.to_date = '9999-01-01'";
+
+            PreparedStatement pstmt = con.prepareStatement(strSelect);
+            pstmt.setInt(1, ID);
+
+            ResultSet rset = pstmt.executeQuery();
+
             if (rset.next())
             {
                 Employee emp = new Employee();
                 emp.emp_no = rset.getInt("emp_no");
                 emp.first_name = rset.getString("first_name");
                 emp.last_name = rset.getString("last_name");
+                emp.title = rset.getString("title");
+                emp.salary = rset.getInt("salary");
+                emp.dept_name = rset.getString("dept_name");
+                emp.manager = rset.getString("manager");
                 return emp;
             }
             else
                 return null;
         }
-        catch (Exception e)
+        catch (SQLException e)
         {
             System.out.println(e.getMessage());
             System.out.println("Failed to get employee details");
             return null;
         }
     }
+
+    /**
+     * Display a single employee
+     */
     public void displayEmployee(Employee emp)
     {
         if (emp != null)
         {
             System.out.println(
-                    emp.emp_no + " "
-                            + emp.first_name + " "
-                            + emp.last_name + "\n"
-                            + emp.title + "\n"
-                            + "Salary:" + emp.salary + "\n"
-                            + emp.dept_name + "\n"
-                            + "Manager: " + emp.manager + "\n");
+                    emp.emp_no + " " +
+                            emp.first_name + " " +
+                            emp.last_name + "\n" +
+                            emp.title + "\n" +
+                            "Salary: " + emp.salary + "\n" +
+                            emp.dept_name + "\n" +
+                            "Manager: " + emp.manager + "\n");
+        }
+    }
+
+    /**
+     * Get all employees and salaries for a given role
+     */
+    public List<Employee> getSalariesByRole(String roleTitle)
+    {
+        List<Employee> employees = new ArrayList<>();
+        try
+        {
+            String sql =
+                    "SELECT e.emp_no, e.first_name, e.last_name, s.salary " +
+                            "FROM employees e " +
+                            "JOIN salaries s ON e.emp_no = s.emp_no " +
+                            "JOIN titles t ON e.emp_no = t.emp_no " +
+                            "WHERE s.to_date = '9999-01-01' " +
+                            "AND t.to_date = '9999-01-01' " +
+                            "AND t.title = ? " +
+                            "ORDER BY e.emp_no ASC";
+
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, roleTitle);
+
+            ResultSet rset = pstmt.executeQuery();
+
+            while (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
+                emp.salary = rset.getInt("salary");
+                employees.add(emp);
+            }
+        }
+        catch (SQLException e)
+        {
+            System.out.println("Error fetching salaries by role: " + e.getMessage());
+        }
+
+        return employees;
+    }
+
+    /**
+     * Display a list of employees and salaries
+     */
+    public void displaySalariesByRole(List<Employee> employees)
+    {
+        System.out.println("EmpNo\tFirst Name\tLast Name\tSalary");
+        for (Employee e : employees)
+        {
+            System.out.printf("%d\t%s\t%s\t%d%n", e.emp_no, e.first_name, e.last_name, e.salary);
         }
     }
 }
